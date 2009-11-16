a656ad220a314ee5d488949dc903a2b8eda56f4c
Kjell-Magne Øierud <kjellm@redpill-linpro.com>
2009-11-16 19:02:21 +0100

    Updated POD

--- lib/MooseX/App/Cmd/Command/BashComplete.pm
+++ lib/MooseX/App/Cmd/Command/BashComplete.pm
@@ -95,65 +95,32 @@ __END__
 
 MooseX::App::Cmd::Command::BashComplete - The great new MooseX::App::Cmd::Command::BashComplete!
 
+
 =head1 VERSION
 
 Version 0.01
 
+
 =head1 SYNOPSIS
 
-Quick summary of what the module does.
+package MyApp::Command::BashComplete;
+use Moose;
+extends 'MooseX::App::Cmd::Command::BashComplete';
 
-Perhaps a little code snippet.
-
-    use MooseX::App::Cmd::Command::BashComplete;
-
-    my $foo = MooseX::App::Cmd::Command::BashComplete->new();
-    ...
 
 =head1 AUTHOR
 
 Kjell-Magne Øierud, C<< <kjellm at acm.org> >>
 
+
 =head1 BUGS
 
-Please report any bugs or feature requests to C<bug-moosex-app-cmd-command-bashcomplete at rt.cpan.org>, or through
-the web interface at L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=MooseX-App-Cmd-Command-BashComplete>.  I will be notified, and then you'll
-automatically be notified of progress on your bug as I make changes.
-
-
-
-
-=head1 SUPPORT
-
-You can find documentation for this module with the perldoc command.
-
-    perldoc MooseX::App::Cmd::Command::BashComplete
-
-
-You can also look for information at:
-
-=over 4
-
-=item * RT: CPAN's request tracker
-
-L<http://rt.cpan.org/NoAuth/Bugs.html?Dist=MooseX-App-Cmd-Command-BashComplete>
-
-=item * AnnoCPAN: Annotated CPAN documentation
-
-L<http://annocpan.org/dist/MooseX-App-Cmd-Command-BashComplete>
-
-=item * CPAN Ratings
-
-L<http://cpanratings.perl.org/d/MooseX-App-Cmd-Command-BashComplete>
-
-=item * Search CPAN
-
-L<http://search.cpan.org/dist/MooseX-App-Cmd-Command-BashComplete/>
-
-=back
-
-
-=head1 ACKNOWLEDGEMENTS
+Please report any bugs or feature requests to
+C<bug-moosex-app-cmd-command-bashcomplete at rt.cpan.org>, or through
+the web interface at
+L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=MooseX-App-Cmd-Command-BashComplete>.
+I will be notified, and then you'll automatically be notified of
+progress on your bug as I make changes.
 
 
 =head1 COPYRIGHT & LICENSE
