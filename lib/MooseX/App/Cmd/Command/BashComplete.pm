5ad4cd2d502f9d8fe59a3ffd4e028a8a02e06d88
Kjell-Magne Øierud <kjellm@acm.org>
2011-03-30 16:52:19 +0200

    Appended the command name as prefix to the generated shell functions and variables

--- lib/MooseX/App/Cmd/Command/BashComplete.pm
+++ lib/MooseX/App/Cmd/Command/BashComplete.pm
@@ -21,61 +21,62 @@ sub execute {
 
     my $cmd_list = join ' ', @commands;
     my $package  = __PACKAGE__;
+    my $prefix = $self->app->arg0;
 
     print <<"EOT";
 #!/bin/bash
 
 # Built with $package;
 
-COMMANDS='help commands bashcomplete $cmd_list'
+${prefix}_COMMANDS='help commands bashcomplete $cmd_list'
 
-_macc_help() {
+_${prefix}_macc_help() {
     if [ \$COMP_CWORD = 2 ]; then
-        _compreply "\$COMMANDS"
+        _compreply "\$${prefix}_COMMANDS"
     else
         COMPREPLY=()
     fi
 }
 
-_macc_commands() {
+_${prefix}_macc_commands() {
     COMPREPLY=()
 }
 
-_macc_bashcomplete() {
+_${prefix}_macc_bashcomplete() {
     COMPREPLY=()
 }
 
 EOT
 
     while (my ($c, $o) = each %command_map) {
-        print "_macc_$c() {\n    _compreply \"",
+        print "_${prefix}_macc_$c() {\n    _compreply \"",
             join(" ", map {"--" . $_->{name}} @$o),
                 "\"\n}\n\n";
     }
 
 
-print <<'EOT';
+print <<"EOT";
 
-_compreply() {
-    COMPREPLY=($(compgen -W "$1" -- ${COMP_WORDS[COMP_CWORD]}))
+_${prefix}_compreply() {
+    COMPREPLY=(\$(compgen -W "\$1" -- \${COMP_WORDS[COMP_CWORD]}))
 }
 
-_macc() {
-    case $COMP_CWORD in
+_${prefix}_macc() {
+    case \$COMP_CWORD in
         0)
             ;;
         1)
-            _compreply "$COMMANDS"
+            _${prefix}_compreply "\$${prefix}_COMMANDS"
             ;;
         *)
-            eval _macc_${COMP_WORDS[1]}
+            eval _${prefix}_macc_\${COMP_WORDS[1]}
             
     esac
 }
 
 EOT
 
-    print "complete -o default -F _macc ", $self->app->arg0, "\n";
+    print "complete -o default -F _${prefix}_macc ", $self->app->arg0, "\n";
 }
 
 
